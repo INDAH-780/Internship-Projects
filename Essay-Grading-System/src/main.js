ef439221c65f798aa5d783b32b79d40a2b2dd895
INDAH-780 <mbahindah780@gmail.com>
2024-10-07 12:15:34 +0100

    deleted inherited folders from main branch which wasn't needed in this branch

--- Essay-Grading-System/src/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.js";
-
-// Instead of JSX, use createElement
-ReactDOM.createRoot(document.getElementById("root")).render(
-  React.createElement(React.StrictMode, null, React.createElement(App))
-);
